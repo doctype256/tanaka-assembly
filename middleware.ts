7ba1d10db2a35784b6f7798286ad6f1e838c508b
hatake <syo0211hatake@gmail.com>
2026-01-22 10:53:38 +0900

    Update matcher to exclude 'api' routes

--- middleware.ts
+++ middleware.ts
@@ -32,6 +32,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next|favicon.ico|.*\\..*).*)'
+    '/((?!_next|api|favicon.ico|.*\\..*).*)'
   ],
 };
