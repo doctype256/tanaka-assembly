2406692234a97f4a2302d673cf62fe9ee2e3d856
hatake <syo0211hatake@gmail.com>
2026-01-27 09:35:30 +0900

    Delete lib directory

--- lib/cases.ts
+++
@@ -1,9 +0,0 @@
-// lib/cases.ts
-import { client } from './turso';
-
-export async function getCases() {
-  const result = await client.execute(
-    'SELECT id, title, content FROM cases'
-  );
-  return result.rows;
-}
