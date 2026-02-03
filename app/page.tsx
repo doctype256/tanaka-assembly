9e8096ac6ea3a17be474af4f5e5d60f82ffdb699
hatake-kamogawa <syo0211hatake@gmail.com>
2026-02-03 14:17:18 +0900

    index

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export default function StaticPage() {
-  const filePath = path.join(process.cwd(), 'public', 'your-file.html');
+  const filePath = path.join(process.cwd(), 'public', 'index.html');
   const htmlContent = fs.readFileSync(filePath, 'utf8');
 
   return (
