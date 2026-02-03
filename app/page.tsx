c3406e0a6164a1889949436bf388e8a0bcebb9cc
hatake-kamogawa <syo0211hatake@gmail.com>
2026-02-03 14:15:52 +0900

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <div>Next.js is running</div>
+import * as fs from 'fs';
+import * as path from 'path';
+
+export default function StaticPage() {
+  const filePath = path.join(process.cwd(), 'public', 'your-file.html');
+  const htmlContent = fs.readFileSync(filePath, 'utf8');
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+  );
 }
