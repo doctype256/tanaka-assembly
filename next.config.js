d811b49b52ccf85cb73cc3bae45e5ce1fe6ebb5e
hatake-kamogawa <syo0211hatake@gmail.com>
2026-02-03 13:40:38 +0900

    16

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
+  // Turbopackを有効にしつつ、エラーを解消する設定
+  experimental: {
+    turbopack: {}
   }
 };
 
