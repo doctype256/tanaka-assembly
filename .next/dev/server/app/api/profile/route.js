439120675155264eebd837a28b1bf7f48a7cc0cb
kawabekeito <kawabe1207keito@gmail.com>
2026-02-04 09:39:42 +0900

    kwb

---
+++ .next/dev/server/app/api/profile/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/profile/route.js")
+R.c("server/chunks/[root-of-the-server]__b4f01898._.js")
+R.c("server/chunks/[root-of-the-server]__6cf26b96._.js")
+R.c("server/chunks/_next-internal_server_app_api_profile_route_actions_0369ef5c.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/profile/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/profile/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
