536d665737101c29051e7e8d7e531de8595f758a
hatake <syo0211hatake@gmail.com>
2026-02-05 14:18:34 +0900

    Delete .next/dev/server/app/api directory

--- .next/dev/server/app/api/contacts/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/contacts/route.js")
-R.c("server/chunks/[root-of-the-server]__8e6bf189._.js")
-R.c("server/chunks/[root-of-the-server]__8f759b41._.js")
-R.c("server/chunks/_next-internal_server_app_api_contacts_route_actions_a51e34c8.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/contacts/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/contacts/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
