0eac8bf4cafa4e52f6e5579cbb380428ed1b9fa8
hatake <syo0211hatake@gmail.com>
2026-02-06 11:20:44 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+
 export default function StaticPage() {
   const filePath = path.join(process.cwd(), 'public', 'index.html');
   const htmlContent = fs.readFileSync(filePath, 'utf8');
